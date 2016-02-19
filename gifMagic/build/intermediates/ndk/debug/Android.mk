e9647010b3d8d5e1e956c2942bd66de19b8e34c6
Nero-Eric-Chen <echen@nero.com>
2016-02-19 13:57:21 +0800

    update

---
+++ gifMagic/build/intermediates/ndk/debug/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ffmpegutils
+LOCAL_SRC_FILES := \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\Android.mk \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\Android.mk \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\ImageFile.cpp \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\libavcodec.a \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\libavcore.a \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\libavdevice.a \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\libavfilter.a \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\libavformat.a \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\libavutil.a \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\libswscale.a \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\VideoFile.cpp \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\VideoFileManager.cpp \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\video\VideoFrame.cpp \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\yuv420sp\Android.mk \
+	E:\GitHub\Android-Projects\gifMagic\src\main\jni\yuv420sp\YUV420SP.c \
+
+LOCAL_C_INCLUDES += E:\GitHub\Android-Projects\gifMagic\src\main\jni
+LOCAL_C_INCLUDES += E:\GitHub\Android-Projects\gifMagic\src\debug\jni
+
+include $(BUILD_SHARED_LIBRARY)
--- gifMagic/src/main/jni/video/Android.mk
+++ gifMagic/src/main/jni/video/Android.mk
@@ -8,8 +8,7 @@ LOCAL_SRC_FILES := VideoFile.cpp \
                  VideoFrame.cpp \
                  ImageFile.cpp
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include \
-                 /home/gshe/android/android-ndk-r6b/platforms/android-8/arch-arm/usr/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -L$(NDK_PLATFORMS_ROOT)/android-9/arch-arm/usr/lib -L$(LOCAL_PATH) -ljnigraphics -lavformat -lavcodec -lavdevice -lavfilter -lavcore -lavutil -lswscale -llog -lz -ldl -lgcc
 
 include $(BUILD_SHARED_LIBRARY)
